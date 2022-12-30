670284d441a2d0dc3bfb8bc301c82796dbc9b5bf
ElMidaouiImane <121026639+ElMidaouiImane@users.noreply.github.com>
2022-12-30 17:33:34 +0100

    Update and rename TP1_analyse.m to Analyse fréquentielle du chant du rorqual bleu.m

--- Analyse fréquentielle du chant du rorqual bleu.m
+++ Analyse fréquentielle du chant du rorqual bleu.m
@@ -4,6 +4,8 @@ clc
 %% ---1---
 [x,fs] = audioread("bluewhale.au");
 chant = x(2.45e4:3.10e4);
+
+
 %% ---2---
 sound(x,fs)
 N=length(chant);
@@ -11,6 +13,8 @@ ts=1/fs;
 t=(0:N-1)*(10*ts);
 plot(t,chant);
 title('Le signal')
+  
+  
 %% ---3---
 dsp_chant = (abs(fft(chant)).^2)/N;
 f = (0:floor(N/2))*(fs/N)/10;
